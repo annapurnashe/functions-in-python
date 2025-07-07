54e4e37bf2612c4edc53eeee38844c63b2f47599
annapurnashe <shembuleannapurna598gmail.com>
2025-07-07 18:19:19 +0530

    first commit

---
+++ default.py
@@ -0,0 +1,15 @@
+def dispvalue(sno,sname,marks,crs="python"):
+    print("\t{} \t{} \t{} \t{} ".format(sno,sname,marks,crs))
+
+print("\tsno \tname \tmarks \tcourse")
+dispvalue(100,"RS",45.32)
+dispvalue(200,"TR",45.5)
+dispvalue(300,"DR",90.0)
+dispvalue(400,"AD",80.88)
+
+
+
+
+
+
+
